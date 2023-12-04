a7dfffc670ffe967b2e8eb79b7323efe6dce4273
Josemar Rodrigues <145208574+Josemar2002@users.noreply.github.com>
2023-12-04 18:42:10 -0300

    introdução ao JavaScript
    primeiro exercício

---
+++ exercicios/assets/js/script.js
@@ -0,0 +1,42 @@
+// let idade = 21;
+
+// if (idade >= 18) {
+//     console.log ("Voçê é maior de idade.");
+// }else {
+//     console.log ("Voçê é menor de idade.");
+// }
+
+// let idade = 21;
+// if (idade === 21) {
+//     console.log("Idade adcionada com sucesso");
+//     console.log(idade);
+// }
+// let idade = 91;
+// if (idade < 18) {
+//     console.log("Voçê é criança")
+// } else if (idade >= 18 && idade < 60){
+//     console.log ("Voçê é um adulto");
+// }else if (idade >= 60) {
+//     console.log("Voçe é idoso")
+// } 
+
+let x = 10;
+let y = 5;
+console.log(x > y);
+
+
+let w = "10";
+let z = 10;
+console.log(w === z);
+
+let a = 10;
+let b = 5;
+console.log(a != b)
+
+
+let preco = 40.3;
+if (preco <= 45) {
+    console.log ("O preço da carne está na promoção")
+}else {
+    console.log ("O preço está caro")
+}
