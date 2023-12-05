55a7c8555275f35ae9a5bf3c94faefd1396f62dc
Josemar Rodrigues <145208574+Josemar2002@users.noreply.github.com>
2023-12-05 18:32:41 -0300

    updates de codigo JS

--- exercicios/assets/js/script.js
+++ exercicios/assets/js/script.js
@@ -20,23 +20,51 @@
 //     console.log("Voçe é idoso")
 // } 
 
-let x = 10;
-let y = 5;
-console.log(x > y);
+// let x = 10;
+// let y = 5;
+// console.log(x > y);
 
 
-let w = "10";
-let z = 10;
-console.log(w === z);
+// let w = "10";
+// let z = 10;
+// console.log(w === z);
 
-let a = 10;
-let b = 5;
-console.log(a != b)
+// let a = 10;
+// let b = 5;
+// console.log(a != b)
 
 
-let preco = 40.3;
-if (preco <= 45) {
-    console.log ("O preço da carne está na promoção")
-}else {
-    console.log ("O preço está caro")
+// let preco = 40.3;
+// if (preco <= 45) {
+//     console.log ("O preço da carne está na promoção")
+// }else {
+//     console.log ("O preço está caro")
+// }
+
+// let isMember = true;
+// let shipping = isMember ? 2 : 10;
+// console.log(isMember ? "Voçê é membro" : "Voçê não é membro");
+// console.log("Frete:" + shipping); 
+
+// let age = 84;
+// let Isdult = (age >= 18  ? "sim" : "Não");
+// console.log(Isdult)
+
+let profession = "bombeiro";
+console.log("Profissão: " + profession);
+
+switch (profession) {
+    case "fiscal" :
+        console.log("Sua camisa será VERDE");
+        break;
+    case "bombeiro":
+        console.log("Sua camisa será VERMELHA");
+        break;
+    case "policial":
+        console.log("Sua camisa será PRETA");
+        break;
+    default:
+        console.log("Sua profissão não estar no sistema")
+        break
+
 }
