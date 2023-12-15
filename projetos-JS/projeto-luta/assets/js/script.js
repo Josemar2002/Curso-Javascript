9602e04d401b4678cb3b30c717a863e568973657
Josemar Rodrigues <145208574+Josemar2002@users.noreply.github.com>
2023-12-15 17:56:30 -0300

    up

---
+++ projetos-JS/projeto-luta/assets/js/script.js
@@ -0,0 +1,14 @@
+let char = new Guerreiro('josemar')
+console.log(char.name)
+let monster = new maximoMonster();
+console.log(monster.name)
+
+const senario = new Senario (
+    char,
+    monster,
+    document.querySelector('#char'),
+    document.querySelector('#monster')
+
+)
+
+senario.start();
