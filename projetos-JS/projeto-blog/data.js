58b6488f3872f1450f38f1b402cb33a02908197e
Josemar Rodrigues <145208574+Josemar2002@users.noreply.github.com>
2024-01-12 18:59:24 -0300

    update

---
+++ projetos-JS/projeto-blog/data.js
@@ -0,0 +1,24 @@
+let d = new Date();
+d.setHours(d.getHours() + 20)
+let novoValor = d
+console.log(novoValor)
+
+//conceitos sobre datas e manipulação de data
+
+/*
+let novoValor = d.getFullYear() = pega o ano
+let novoValor = d.getMonth() = pega o mês
+let novoValor = d.getDay() = retorna em número o dia da semana começando pelo domingo
+let novoValor = d.getDate() = pega o dia atual
+let novoValor = d.getHours() = pega as horas
+let novoValor = d.getMinutes() = pega os minutos
+let novoValor = d.getSeconds() = pega os segundos
+let novoValor = d.getMilliseconds() = pega os milesegundos
+let novoValor = d.getTime() = retorna a quantidade milesegundos que se passaram desde de 1970 até a data atual  {faz a mesma coisa [Date.now()]}
+
+d.setFullYear(2022) = para fazer alterção do ano 
+assim sucefivamente APENAS TROQUE O GET PELO SET E FARA ALTERAÇÃO DE DADOS EM DATAS
+
+d.setDate(d.getDate() + 5) == acresenta aos dias normais 5 dias
+d.setHours(d.getHours() + 20) = seta a hora e aumenta de acordo com oque voçê decide
+*/
