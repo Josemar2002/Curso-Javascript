a71eba0ec47ff988375af8202a3be925fd7fe9d1
Josemar Rodrigues <145208574+Josemar2002@users.noreply.github.com>
2024-01-22 20:19:35 -0300

    update

--- projetos-JS/projeto-blog/requisicao03.js
+++ projetos-JS/projeto-blog/requisicao03.js
@@ -1,13 +1,38 @@
-async function enviar() {
-    let arquivo = document.getElementById('arquivo').files[0]
-    let body = new FormData()
-    body.append('title', 'Pantera cor de rosa')
-    body.append('arquivo', arquivo)
-    let req = await fetch('https://www.josemarRodrigues.com.br/upload', {
-        method: 'POST',
-        body: body,
-        headers: {
-            'Content-Type' : 'multipart/form-data'
-        }
-    })
+// async function enviar() {
+//     let arquivo = document.getElementById('arquivo').files[0]
+//     let body = new FormData()
+//     body.append('title', 'Pantera cor de rosa')
+//     body.append('arquivo', arquivo)
+//     let req = await fetch('https://www.josemarRodrigues.com.br/upload', {
+//         method: 'POST',
+//         body: body,
+//         headers: {
+//             'Content-Type' : 'multipart/form-data'
+//         }
+//     })
+// }
+
+//mostrar imagens 
+//(sicrono)
+/*
+function mostrar() {
+    let imagem = document.getElementById("imagem").files[0]  
+    let img = document.createElement('img')
+    img.src = URL.createObjectURL(imagem)
+    document.getElementById("posts").appendChild(img)
 }
+*/
+
+function mostrar() {
+    let reader = new FileReader()
+    let imagem = document.getElementById('imagem').files[0]
+    reader.onloadend = function() {
+        let img = document.createElement('img')
+        img.src = reader.result
+        img.width = 200
+        document.getElementById('posts').appendChild(img)
+    }
+    reader.readAsDataURL(imagem)
+}
+
+
