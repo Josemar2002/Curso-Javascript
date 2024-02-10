4adcf4cbd32fff2fd025ec151111f530cd07602a
Josemar Rodrigues <145208574+Josemar2002@users.noreply.github.com>
2024-02-10 19:51:28 -0300

    update

---
+++ projetos-JS/Projetos-Finais/urna/etapas.js
@@ -0,0 +1,50 @@
+let etapas = [
+    {
+        titulo: 'VEREADOR',
+        numeros: 5,
+        candidatos: [
+            {
+                numero: '38111',
+                nome: 'Pica pau',
+                partido: 'ABC',
+                 fotos: [
+                    {url:'38111.jpg', legenda:'Vereador'}
+                 ]
+            },
+            {
+                numero: '77222',
+                nome: 'Tio patinhas',
+                partido: 'DFT',
+                 fotos: [
+                    {url:'77222.jpg', legenda:'Vereador'}
+                 ]
+            }
+        ]
+    },
+    {
+        titulo: 'PREFEITO',
+        numeros: 2,
+        candidatos: [
+            {
+                numero: '99',
+                nome: 'Ciclano',
+                partido: 'ABC',
+                vice: 'Cic',
+                 fotos: [
+                    {url:'99.jpg', legenda:'Prefeito'},
+                    {url:'99_2.jpg', legenda:'Vereador', small: true}
+                 ]
+            },
+            {
+                numero: '84',
+                nome: 'Souza',
+                partido: 'DFT',
+                vice: 'Bet',
+                 fotos: [
+                    {url:'84.jpg', legenda:'Prefeito'},
+                    {url:'84_2.jpg', legenda:'Vereador', small: true}
+                 ]
+            } 
+        ]
+    }
+]
